01429a754edfe3540a5f9a490b5dad09e330ff0a
steffilamarca <steffi@la-marca.com>
2024-07-09 16:55:14 +0800

    Implemented Supplement Page

--- src/components/events/Supplement.js
+++ src/components/events/Supplement.js
@@ -4,25 +4,33 @@ import supplements from "./../../assets/data/supplements.json"
 
 const Supplement = () => {
 
-    const { id } = useParams(); // Destructure id from useParams
+    const { id } = useParams();
+    const supplement = supplements.find(supplement => supplement.supplementId === Number(id));
 
-    // // Find the supplement with the matching supplementId
-    const supplement = supplements.find(supplement => supplement.supplementId === id);
-
-    return(<div className="supplement-page px-3 px-lg-5 mb-4">
-        <div className="breadcrumb px-3 py-2 rounded-2">You are here:&nbsp;<NavLink exact to="/">Home</NavLink> &nbsp; &gt; &nbsp;Events &gt; &nbsp; <NavLink exact to="/events/mediabuzz">MediaBUZZ Events</NavLink> &nbsp; &gt; &nbsp; <span className="breadcrumb-selected">Supplements</span></div>
+    return(<div className="supplement-page w-75 mx-auto px-3 px-lg-5 mb-5">
+        <div className="breadcrumb px-3 py-2 rounded-2">You are here:&nbsp;<NavLink exact to="/">Home</NavLink> &nbsp; &gt; &nbsp;Events &gt; &nbsp; <NavLink to="/events/mediabuzz">MediaBUZZ Events</NavLink> &nbsp; &gt; &nbsp; <NavLink to="/events/mediabuzz/supplements">Supplements</NavLink> &nbsp; &gt; &nbsp; <span className="breadcrumb-selected">{supplement.title}</span></div>
         <h2 className="page-heading">{supplement.title}</h2>
         <img className="supplement-image ms-4" src={`/images/supplements/supplement-${supplement.supplementId}.webp`}></img>
         <hr className="text-muted"/>
-        {supplement.description.text.split("\n\n").map((paragraph, index) => (
+        {supplement.description.split("\n\n").map((paragraph, index) => (
             <p className="mb-3" key={index}>{paragraph}</p>
         ))}
-        {supplement.links && <ul>
+        {supplement.links && <ul className="mb-4">
             {supplement.links.map(link => <li>
-                {link.pdf ? <a href={`/pdfs/supplements/${link.pdf}.pdf`} target="_blank" rel="noopener noreferrer">{link.title}</a> : <p>{link.title}</p>}
+                {link.pdf ? <a href={`/pdfs/supplements/${link.pdf}.pdf`} target="_blank" rel="noopener noreferrer">{link.name}</a> : <p>{link.name}</p>}
                 {link.description && <p>{link.description}</p>}
             </li>)}
         </ul>}
+        {supplement.credits && <div className="row">
+            {supplement.credits.map(credit => <div className="col-4 text-center my-auto">
+                <a href={credit.link} target="_blank" rel="noopener noreferrer">
+                    <img src={`/images/supplements/credits/${credit.image}.webp`}></img>
+                </a>
+                {credit.description.split("\n\n").map((information, index) => (
+                    <p className="" key={index}>{information}</p>
+                ))}
+            </div>)}
+        </div>}
     </div>)
 }
 
--- src/components/events/Supplements.js
+++ src/components/events/Supplements.js
@@ -2,15 +2,15 @@ import { NavLink } from "react-router-dom";
 import supplements from "./../../assets/data/supplements.json"
 
 const Supplements = () => {
-    return(<div className="supplements-page px-3 px-lg-5">
-        <div className="breadcrumb px-3 py-2 rounded-2">You are here:&nbsp;<NavLink exact to="/">Home</NavLink> &nbsp; &gt; &nbsp;Events &gt; &nbsp; <NavLink exact to="/events/mediabuzz">MediaBUZZ Events</NavLink> &nbsp; &gt; &nbsp; <span className="breadcrumb-selected">Supplements</span></div>
-        <div className="row mb-4">
-            {supplements.map(supplement=><div className="col-6">
+    return(<div className="supplements-page w-75 mx-auto px-3 px-lg-5">
+        <div className="breadcrumb px-3 py-2 rounded-2">You are here:&nbsp;<NavLink exact to="/">Home</NavLink> &nbsp; &gt; &nbsp;Events &gt; &nbsp; <NavLink to="/events/mediabuzz">MediaBUZZ Events</NavLink> &nbsp; &gt; &nbsp; <span className="breadcrumb-selected">Supplements</span></div>
+        <div className="row mb-3">
+            {supplements.map(supplement=><div className="col-6 mb-4">
                 <h4 className="supplement-title-thumbnail">{supplement.title}</h4>
                 <div className="supplement-thumbnail-wrapper me-2">
                     <img className="supplement-thumbnail" src={`/images/supplements/thumbnails/supplement-${supplement.supplementId}.webp`}></img>
                 </div>
-                <p>{supplement.description.text.substring(0, 308)}...</p>
+                <p>{supplement.description.substring(0, 308)}...</p>
                 <NavLink to={`/events/mediabuzz/supplements/${supplement.supplementId}`} className="read-more-btn btn btn-light">Read more...</NavLink>
             </div>)}
         </div>
