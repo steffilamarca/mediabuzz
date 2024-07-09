16591e4f1d13f74468165bd1b074cec78afd2aeb
steffilamarca <steffi@la-marca.com>
2024-07-09 19:32:01 +0800

    Implemeted Advertising Page

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,9 +2,9 @@ import { NavLink } from "react-router-dom";
 
 const AboutUs = () => {
 
-    return(<div className="about-us px-3 px-lg-5">
+    return(<div className="about-us px-3 px-lg-5 mb-4   ">
         <div className="breadcrumb px-3 py-2 rounded-2">You are here:&nbsp;<NavLink exact to="/">Home</NavLink> &nbsp; &gt; &nbsp;<NavLink to="/advert">Advertising</NavLink> &nbsp; &gt; &nbsp; <span className="breadcrumb-selected">About Us</span></div>
-        <p>For our detailed price list and more information, please contact our sales team: sales@mediabuzz.com.sg  or aem@mediabuzz.com.sgOffice line: +65 6836 1807  </p>
+        <p>For our detailed price list and more information, please contact our sales team: <a href="mailto:sales@mediabuzz.com.sg">sales@mediabuzz.com.sg</a> or <a href="mailto:aem@mediabuzz.com.sg">aem@mediabuzz.com.sg</a>. Office line: +65 6836 1807  </p>
     </div>)
 }
 
--- src/components/Advertising.js
+++ src/components/Advertising.js
@@ -1,11 +1,110 @@
+import React, { useState, useRef, useEffect } from 'react';
 import { NavLink } from "react-router-dom";
+import HTMLFlipBook from 'react-pageflip';
+
+const PageCover = React.forwardRef((props, ref) => {
+
+    return (
+        <div className="page page-cover" ref={ref} data-density="soft">
+        <div className="page-content">
+            <h2>{props.children}</h2>
+        </div>
+        </div>
+    );
+    });
+
+const Page = React.forwardRef((props, ref) => {
+  return (
+    <div className="page" ref={ref}>
+        <div className="book-page-wrapper">
+            <img className="book-page" src={`/images/eMags/2022/page-${props.number}.png`} alt={`Asian Marketing Page ${props.number}`}></img>
+        </div>
+    </div>
+  );
+});
 
 const Advertising = () => {
-    return(<div className="advertising px-3 px-lg-5">
+
+    const pages = Array.from({ length: 10 }, (_, index) => index + 1); // Create an array from 1 to 10
+    
+
+    const [page, setPage] = useState(0);
+    const [totalPage, setTotalPage] = useState(0);
+    const flipBookRef = useRef(null);
+
+    const nextButtonClick = () => {
+        const flipBook = flipBookRef.current;
+        if (flipBook && flipBook.flipNext) {
+        flipBook.flipNext();
+        }
+    };
+
+    const prevButtonClick = () => {
+        const flipBook = flipBookRef.current;
+        if (flipBook && flipBook.flipPrev) {
+        flipBook.flipPrev();
+        }
+    };
+
+    const onPage = e => {
+        setPage(e.data);
+    };
+
+    useEffect(() => {
+        const flipBook = flipBookRef.current;
+        if (flipBook && flipBook.getPageFlip) {
+        setTotalPage(flipBook.getPageFlip().getPageCount());
+        }
+    }, []);
+
+  return (
+    <div className="advertising px-3 px-lg-5 mb-4">
         <div className="breadcrumb px-3 py-2 rounded-2">You are here:&nbsp;<NavLink exact to="/">Home</NavLink> &nbsp; &gt; &nbsp;<span className="breadcrumb-selected">Advertising</span></div>
-        <h2 className="media-kit">MEDIA KIT 2022</h2>
-        <h6>Category: <NavLink to="/about-us">About Us</NavLink></h6>
-    </div>)
-}
+        <h2 className="page-heading">MEDIA KIT 2022</h2>
+        <h6>Also view: <NavLink to="/about-us">About Us</NavLink></h6>
+        <div className="demo-book">
+            <HTMLFlipBook
+            width={550}
+            height={733}
+            size="stretch"
+            minWidth={315}
+            maxWidth={1000}
+            minHeight={400}
+            maxHeight={1533}
+            maxShadowOpacity={0.5}
+            showCover={true}
+            mobileScrollSupport={true}
+            onFlip={onPage}
+            ref={flipBookRef}
+            >
+            <PageCover>
+                <div className="book-cover-wrapper">
+                    <img className="book-cover" src="/images/eMags/2022/cover.png" alt="2022 Asian Marketing Cover"></img>
+                </div>
+            </PageCover>
+            {pages.map((pageNumber) => (
+                <Page key={pageNumber} number={pageNumber} />
+                ))}
+            <PageCover>
+                <div className="book-backpage-wrapper">
+                    <img className="book-backpage" src="/images/eMags/backpage.png" alt="Asian Marketing Backpage"></img>
+                </div>
+            </PageCover>
+            </HTMLFlipBook>
+        </div>
+        {/* <div className="container">
+            <div>
+            <button type="button" onClick={prevButtonClick}>
+                Previous page
+            </button>
+            [<span>{page}</span> of <span>{totalPage}</span>]
+            <button type="button" onClick={nextButtonClick}>
+                Next page
+            </button>
+            </div>
+        </div> */}
+    </div>
+  );
+};
 
 export default Advertising;
