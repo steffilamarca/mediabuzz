5d0a3bee974229a28a323a9eeac9d78d4918b9d8
steffilamarca <steffi@la-marca.com>
2024-07-09 19:38:05 +0800

    Updated Advertisin.js

--- src/components/Advertising.js
+++ src/components/Advertising.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { NavLink } from "react-router-dom";
 import HTMLFlipBook from 'react-pageflip';
 
